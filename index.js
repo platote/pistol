d349824c66ba4b0fb3bd2ea9cdfb51d9257f0582
platote <84842352+platote@users.noreply.github.com>
2022-03-01 14:36:03 +0100

    update

--- index.js
+++ index.js
@@ -128,7 +128,7 @@ export default e => {
     }
     
     {
-      let u2 = `${baseUrl}military.glb`;
+      let u2 = `${baseUrl}rifle.glb`;
       if (/^https?:/.test(u2)) {
         u2 = '/@proxy/' + u2;
       }
